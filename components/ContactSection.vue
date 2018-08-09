87fc0eceaa2b668083a8e8e149ce51aa5308fc53
isan <38833845+isanbitoy@users.noreply.github.com>
2018-08-09 23:47:18 +0800

    Update ContactSection.vue

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -5,7 +5,7 @@
       <div id="map" class="map"></div>
 		<form id="contact" action="https://formspree.io/isanbitoy@gmail.com" method="POST">
     		<fieldset>
-      			<input name="name" placeholder="Name" type="text" tabindex="1" required>
+      			<div><input name="name" placeholder="Name" type="text" tabindex="1" required></div>
     		</fieldset>
 
     		<fieldset>
