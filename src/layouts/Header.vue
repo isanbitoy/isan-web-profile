e4caa2dcdf8a84aa67b0b819d2dfbe8c678d58a4
isan <isanbitoy@gmail.com>
2019-06-09 15:00:14 +0800

    add files

---
+++ src/layouts/Header.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="header-background">
+	<div class="skewed"></div>
+	
+	<section id="header-layout">
+		<div class="navigation__bar">
+			<div>
+				<figure class="img__box">
+					<g-image class="nav-img" src="~/assets/img/author.png" />
+				</figure>
+				<div class="info__box">
+					<h1 class="author__name">LISANDRO BITOY</h1>
+					<ul>
+						<li>
+							<span data-tooltip="My Resume">
+								<a :href="resumeLink" target="_blank">Resume</a>
+							</span>
+						</li>
+						<li>
+							<span data-tooltip="My Blog">
+								<a href="" target="_blank">Blog site</a>
+							</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => {
+		return {
+			resumeLink: '../files/Lisandro_A_Bitoy_Resume.pdf'
+		}
+	}
+	/*
+	methods: {
+		onScroll: function() {
+			let headerNav = document.querySelector('.navigation__bar')
+			if (window.scrollY >= 200) {
+				headerNav.classList.add('scrolled')
+			}
+			else {
+				headerNav.classList.remove('scrolled')
+			}
+		}
+	},
+	created: function() {
+		if (process.browser) {
+			window.addEventListener('scroll', this.onScroll)
+		}
+	},
+	destroyed: function() {
+		if (process.browser) {
+			window.removeEventListener('scroll', this.onScroll)
+		}
+	}
+	*/
+};
+</script>
---
+++ src/plugins/globalComponents/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import HeaderLayout from '~/layouts/Header'
+import FooterLayout from '~/layouts/Footer'
+
+Vue.component('header-layout', HeaderLayout)
+Vue.component('footer-layout', FooterLayout)
