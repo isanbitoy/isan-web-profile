6dc2a4d7a5ce4fb8ccd35e7e11918bf9b3d88427
isan <isanbitoy@gmail.com>
2019-06-10 20:44:39 +0800

    edit file

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,31 +1,30 @@
 <template>
 	<div class="header-background">
-	<div class="skewed"></div>
-	
-	<section id="header-layout">
-		<div class="navigation__bar">
-			<div>
-				<figure class="img__box">
-					<g-image class="nav-img" src="~/assets/img/author.png" />
-				</figure>
-				<div class="info__box">
-					<h1 class="author__name">LISANDRO BITOY</h1>
-					<ul>
-						<li>
-							<span data-tooltip="My Resume">
-								<a :href="resumeLink" target="_blank">Resume</a>
-							</span>
-						</li>
-						<li>
-							<span data-tooltip="My Blog">
-								<a href="" target="_blank">Blog site</a>
-							</span>
-						</li>
-					</ul>
+		<div class="blur-background"></div>
+		<section id="header-layout">
+			<div class="navigation__bar">
+				<div>
+					<figure class="img__box">
+						<g-image class="nav-img" src="~/assets/img/author.png" />
+					</figure>
+					<div class="info__box">
+						<h1 class="author__name">LISANDRO BITOY</h1>
+						<ul>
+							<li>
+								<span data-tooltip="My Resume">
+									<a :href="resumeLink" target="_blank">Resume</a>
+								</span>
+							</li>
+							<li>
+								<span data-tooltip="My Blog">
+									<a href="" target="_blank">Blog site</a>
+								</span>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 	</div>
 </template>
 
