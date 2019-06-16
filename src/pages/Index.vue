f2395246ff6955f4ffceaa15c516149e6878fcdd
isan <isanbitoy@gmail.com>
2019-06-17 00:11:45 +0800

    add repo

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+	<Layout>
+		<about-section />
+		<portfolio-section />
+		<contact-section />
+	</Layout>
+</template>
+
+<script>
+import AboutSection from '~/sections/About'
+import PortfolioSection from '~/sections/Portfolio'
+import ContactSection from '~/sections/Contact'
+
+export default {
+	metaInfo: {
+		title: 'Welcome'
+	},
+	components: {
+		'about-section': AboutSection,
+		'portfolio-section': PortfolioSection,
+		'contact-section': ContactSection
+	}
+};
+</script>
---
+++ src/sections/Portfolio.vue
@@ -0,0 +1,36 @@
+<template>
+	<section id="portfolio-section">
+		<div class="grid__layout">
+			<h2>Portfolio</h2>
+			<ul>
+				<li v-for="(project, index) in projectData" :key="index">
+					<a :href="project.url" target="_blank">
+						<article>
+							<figure class="box__figure">
+								<i class="fa fa-link"></i>
+								<img :src="project.img ? project.img : placeHolder" />
+							</figure>
+							<div class="box__info">
+								<h4 class="content--title">{{ project.projTitle }}</h4>
+								<p class="content--desc">{{ project.desc }}</p>
+							</div>
+						</article>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+</template>
+
+<script>
+import projectData from '~/assets/data/project.json'
+
+export default {
+	data: () => {
+		return {
+			placeHolder: 'https://drive.google.com/uc?id=1owlFVdbySkDsFB_unLU612Zz8o-9Vwhf',
+			projectData
+		}
+	}
+};
+</script>
