364235b0435c7bc0071e1965f675a7bdc0ac416d
isan <isanbitoy@gmail.com>
2019-06-17 00:09:49 +0800

    clean repo

--- src/plugins/globalComponents/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import HeaderLayout from '~/layouts/Header'
-import FooterLayout from '~/layouts/Footer'
-
-Vue.component('header-layout', HeaderLayout)
-Vue.component('footer-layout', FooterLayout)
