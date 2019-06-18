69e841054bef0b12b01a0982d1f1ea95c3fd29d2
isan <isanbitoy@gmail.com>
2019-06-19 00:05:49 +0800

    add repo

---
+++ src/layouts/Default.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="default__layout">
+		<transition name="slide_up" appear>
+			<header-layout />
+		</transition>
+		<slot />
+		<footer-layout />
+	</div>
+</template>
+
+<static-query>
+query {
+	metaData {
+		siteName
+	}
+}
+</static-query>
