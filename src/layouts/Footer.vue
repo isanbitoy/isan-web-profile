7d6d38ad1981a0ed3fffd8ed7041575c6e667774
isan <isanbitoy@gmail.com>
2020-01-21 22:39:13 +0800

    add files

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+	<footer>
+		<span><small>&copy;</small></span>
+		<span><small>{{ year }}</small></span>
+		<span><small>{{ author }}</small></span>
+		<span><small>{{ rights }}</small></span>
+	</footer>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			year: new Date().getFullYear(),
+			author: 'Lisandro A. Bitoy.',
+			rights: 'All Rights Reserved.'
+		}
+	}
+};
+</script>
---
+++ src/layouts/Header.vue
@@ -0,0 +1,47 @@
+<template>
+	<header>
+		<nav id="nav-items" class="nav__parent">
+			<a class="nav__child" href="/">Home</a>
+			<a class="nav__child" href="#portfolio">Portfolio</a>
+	        <a class="nav__child" href="#contact">Contact</a>
+	        <a class="nav__child" href="" target="_blank">Resume</a>
+	        <a href="javascript:void(0);" class="icon" v-on:click="openNav">
+				<i class="fa fa-bars"></i>
+			</a>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	methods: {
+		onScroll: function() {
+			let header_obj = document.querySelector('header')
+			if (window.scrollY >= 200) {
+				header_obj.classList.add('materialize')
+			}
+			else {
+				header_obj.classList.remove('materialize')
+			}
+		},
+		openNav: function() {
+			let x = document.querySelector('#nav-items')
+			if (x.className === 'nav__parent') {
+				x.className += ' responsive'
+			} else {
+				x.className = 'nav__parent'
+			}
+		}
+	},
+	created: function() {
+		if (process.browser) {
+			window.addEventListener('scroll', this.onScroll)
+		}
+	},
+	destroyed: function() {
+		if (process.browser) {
+			window.removeEventListener('scroll', this.onScroll)
+		}
+	}
+};
+</script>
