cd7ec252cb6ff50864985951fd2022acec918fc2
isan <isanbitoy@gmail.com>
2020-02-04 19:50:13 +0800

    add files

---
+++ gridsome.config.js
@@ -0,0 +1,15 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  	siteName: 'Lisandro',
+  	plugins: [{
+  		use: '@gridsome/plugin-google-analytics',
+  		options: {
+  			id: 'UA-156593854-1'
+  		}
+  	}]
+}
