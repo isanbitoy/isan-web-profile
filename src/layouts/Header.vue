04b54bfbbcdde24a6447feb13fd1cb9d36adb42d
isan <isanbitoy@gmail.com>
2020-02-04 19:16:39 +0800

    add files

---
+++ src/layouts/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<transition name="slide_down" appear>
+		<header id="header">
+			<div class="header__layout">
+				<figure class="img__box">
+					<g-link to="/">
+						<img class="home-icon" src="@/assets/svg/home.svg" />
+						<img class="nav-img" src="/img/me.png" />
+					</g-link>
+				</figure>
+				<nav class="nav__parent">
+					<g-link to="/portfolio">
+						<a class="nav__child">
+							Portfolio
+						</a>
+					</g-link>
+					<g-link to="/contact">
+						<a class="nav__child">
+							Contact
+						</a>
+					</g-link>
+				</nav>
+			</div>
+		</header>
+	</transition>
+</template>
